579fcc2fe9016fa8c3f37d4a9668c25a4a630327
Lauren Young <66131398+laurenyoung88@users.noreply.github.com>
2022-09-27 17:51:20 -0500

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -20,7 +20,7 @@ from kafka import KafkaConsumer  # consumer of events
 
 # acquire the consumer
 # (you will need to change this to your bootstrap server's IP addr)
-consumer = KafkaConsumer (bootstrap_servers="localhost:9092")
+consumer = KafkaConsumer (bootstrap_servers="129.114.25.235:9092")
 
 # subscribe to topic
 consumer.subscribe (topics=["utilizations"])
